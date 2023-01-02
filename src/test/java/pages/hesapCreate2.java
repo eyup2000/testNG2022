03894a639be8f67aba8b45ffcf2057c3793b42a7
eyup2000 <gulerr.eyup47@gmail.com>
2023-01-02 15:43:12 +0300

    Alistirmaya Devam

---
+++ src/test/java/pages/hesapCreate2.java
@@ -0,0 +1,99 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class hesapCreate2 {
+
+    public hesapCreate2() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(xpath = "//a[@class='call']")
+    public WebElement onaylama;
+
+    @FindBy(id = "pfe-navigation__account-toggle")
+    public WebElement login;
+
+    @FindBy ( xpath = "//a[@class='account-register-link']")
+    public WebElement registerNow;
+
+    @FindBy(xpath = "//input[@id='corporate']")
+    public WebElement buttonTiklama;
+
+    @FindBy(xpath = "//input[@id='username']")
+    public WebElement userName;
+
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement password;
+
+    @FindBy(xpath = "//input[@id='confirmPassword']")
+    public WebElement confirmPassword;
+
+
+    @FindBy(xpath = "(//h2)[7]")
+    public WebElement assert1;
+
+
+
+    @FindBy(xpath = "//input[@id='firstName']")
+    public WebElement firstName;
+
+    @FindBy(xpath = "//input[@id='lastName']")
+    public WebElement lastName;
+
+    @FindBy(xpath = "//input[@id='user.attributes.jobTitle']")
+    public WebElement jobTitle;
+
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement email;
+
+    @FindBy(xpath = "(//input[@type='text'])[6]")
+    public WebElement phoneNumber;
+
+
+    @FindBy(xpath = "(//h2)[9]")
+    public WebElement assert2;
+
+
+    @FindBy(xpath = "//input[@id='user.attributes.company']")
+    public WebElement CompanyName;
+
+    @FindBy(xpath = "//select[@id='user.attributes.country']")
+    public WebElement country;
+
+    @FindBy(xpath = "//input[@id='user.attributes.addressLine1']")
+    public WebElement adress1;
+
+    @FindBy(xpath = "//input[@id='user.attributes.addressLine2']")
+    public WebElement adress2;
+
+    @FindBy(xpath = "//input[@id='user.attributes.addressLine3']")
+    public WebElement adress3;
+
+    @FindBy(xpath = "//input[@id='user.attributes.addressLine4']")
+    public WebElement adress4;
+
+    @FindBy(xpath = "//input[@id='user.attributes.addressPostalCode']")
+    public WebElement postaCode;
+
+    @FindBy(xpath = "//input[@id='user.attributes.addressCityText']")
+    public WebElement city;
+
+    @FindBy(xpath = "//input[@id='user.attributes.addressStateText']")
+    public WebElement state;
+
+    @FindBy(xpath = "//input[@id='user.attributes.addressCountyText']")
+    public WebElement country2;
+
+    @FindBy(xpath = "//button[@id='register-show-step2']")
+    public WebElement createButton;
+
+
+
+
+
+}
---
+++ src/test/java/tests/alistirma/A2.java
@@ -0,0 +1,106 @@
+package tests.alistirma;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.hesapCreate2;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class A2 {
+    hesapCreate2 hesapCreate2 = new hesapCreate2();
+
+    Faker faker= new Faker();
+
+    @BeforeMethod
+    public void setUp(){
+        Driver.getDriver().get(ConfigReader.getProperty("app_url_alistirma2"));
+
+    }
+
+    @Test
+    public void Alisitrma2() throws InterruptedException {
+
+
+        //hesapCreate2.onaylama.click();
+        Thread.sleep(7000);
+        hesapCreate2.login.click();
+
+        hesapCreate2.registerNow.click();
+
+        hesapCreate2.buttonTiklama.click();
+
+        hesapCreate2.userName.sendKeys(faker.name().username());
+
+        hesapCreate2.password.sendKeys("qMPQdUr9A*?zTGD");
+
+        hesapCreate2.confirmPassword.sendKeys("qMPQdUr9A*?zTGD");
+
+        Assert.assertTrue(hesapCreate2.assert1.getText().contains("Personal information"));
+        System.out.println(hesapCreate2.assert1.getText());
+
+        hesapCreate2.firstName.sendKeys(faker.name().firstName());
+
+        hesapCreate2.lastName.sendKeys(faker.name().lastName());
+
+        hesapCreate2.jobTitle.sendKeys(faker.job().title());
+
+        hesapCreate2.email.sendKeys(faker.internet().emailAddress());
+
+        hesapCreate2.phoneNumber.sendKeys(faker.phoneNumber().cellPhone());
+
+        System.out.println(hesapCreate2.assert2.getText());
+        Assert.assertTrue(hesapCreate2.assert2.getText().contains("Company information"));
+
+        hesapCreate2.CompanyName.sendKeys(faker.name().name());
+
+        Select select = new Select(hesapCreate2.country);
+        select.selectByValue("TR");
+        Thread.sleep(2000);
+
+        hesapCreate2.adress1.sendKeys(faker.address().fullAddress());
+
+        hesapCreate2.adress2.sendKeys(faker.address().fullAddress());
+
+        hesapCreate2.adress3.sendKeys(faker.address().fullAddress());
+
+        hesapCreate2.adress4.sendKeys(faker.address().fullAddress());
+
+        hesapCreate2.postaCode.sendKeys(faker.address().zipCode());
+        Thread.sleep(2000);
+
+        hesapCreate2.city.sendKeys(faker.address().city());
+
+        hesapCreate2.state.sendKeys(faker.address().state());
+        hesapCreate2.country2.sendKeys(faker.address().country());
+
+        hesapCreate2.createButton.click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
