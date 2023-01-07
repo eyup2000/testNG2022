2db4e6ec0105fce843cf8b43f0b1fa3dfeb91981
eyup2000 <gulerr.eyup47@gmail.com>
2023-01-07 10:58:05 +0300

    excelUtil

--- src/test/java/excelAutomation/Day13_C14_LoginSmokeTest.java
+++ src/test/java/excelAutomation/Day13_C14_LoginSmokeTest.java
@@ -1,5 +1,6 @@
 package excelAutomation;
 
+import org.testng.annotations.Test;
 import pages.DefaultPage;
 import pages.LoginPage;
 import utilities.ConfigReader;
@@ -25,6 +26,75 @@ public class Day13_C14_LoginSmokeTest {
     public void SetUp(){
         Driver.getDriver().get(ConfigReader.getProperty("app_url_login"));
         loginPage = new LoginPage();
+
+//       if (loginPage.advancedLink.isDisplayed()){
+//           loginPage.advancedLink.click();
+//           loginPage.proceedLink.click();
+//       }
+       try{
+           loginPage.advancedLink.click();
+           loginPage.proceedLink.click();
+       }catch (Exception e){
+           System.out.println("Advenced linki görünmedi");
+       }
+
+    }
+    @Test
+    public void adminLoginTest(){
+        //1.yol
+ //       SetUp();
+//        loginPage.userName.sendKeys(ConfigReader.getProperty("admin_username"));
+//        loginPage.password.sendKeys(ConfigReader.getProperty("admin_password"));
+//        loginPage.loginButton.click();
+
+        //2.yol
+//        SetUp();
+//        loginPage.userName.sendKeys("manager");
+//        loginPage.password.sendKeys("Manager1!");
+//        loginPage.loginButton.click();
+
+        //3.yol
+        String path ="./src/test/java/resources/smoketestdata.xlsx";
+        String sheetName = "admin_login_info";
+
+        excelUtil = new ExcelUtil(path,sheetName);
+
+        testData =  excelUtil.getDataList();
+
+        System.out.println(testData);//[{password=Arcane123!, username=admin}]
+
+
+
+        for (Map<String,String> herData : testData){
+            SetUp();
+        loginPage.userName.sendKeys(herData.get("username"));
+        loginPage.password.sendKeys(herData.get("password"));
+        loginPage.loginButton.click();
+        }
+
+
+
+
+    }
+    @Test
+    public void managerLoginTest(){
+        String path ="./src/test/java/resources/smoketestdata.xlsx";
+        String sheetName = "manager_login_info";
+
+        excelUtil = new ExcelUtil(path,sheetName);
+
+        testData =  excelUtil.getDataList();
+
+        System.out.println(testData);///[{password=Manager1!, username=manager}, {password=Manager5!, username=manager5}, {password=Manager12!, username=manager12}]
+
+
+        for (Map<String,String> herData : testData){
+            SetUp();
+            loginPage.userName.sendKeys(herData.get("username"));
+            loginPage.password.sendKeys(herData.get("password"));
+            loginPage.loginButton.click();
+        }
+
 
     }
 }
